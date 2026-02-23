8b3f0c168834916fd278d350362349d869106028
co77o <ezra77453@gmail.com>
2026-02-23 17:19:42 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/index.js
+++ main/app/(tabs)/index.js
@@ -1,11 +1,12 @@
 import React,{useState} from 'react'
-import {Text,View,StyleSheet} from 'react-native'
+import {Text,View,StyleSheet,TouchableOpacity} from 'react-native'
 
 
 function App(){
   return(
     <View style={{width:'100%',height:'100%',backgroundColor:'blue',alignItems:'center',justifyContent:'center'}}>
       <Text style={{color:'white'}}>hello welcome to react</Text>
+    <TouchableOpacity style={{backgroundColor:'red',borderRadius:8}}><Text style={{color:'white'}}>Hello</Text></TouchableOpacity>
     </View>
     )
 }
