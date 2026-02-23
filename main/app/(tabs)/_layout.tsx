40152baa5b0cfa38d294ad2ea38a0ab310d15c69
co77o <ezra77453@gmail.com>
2026-02-23 20:15:36 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/_layout.tsx
+++ main/app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
 import { Stack } from 'expo-router';
 import React from 'react';
-import {Tabs} from 'expo-router'
+import {Stack} from 'expo-router'
 export default function Layout() {
   return (
-  <Tabs/>
+  <Stack/>
   );
 }
