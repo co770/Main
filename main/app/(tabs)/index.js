8f242c7acad229d8fda54931d36dfa4907254192
co77o <ezra77453@gmail.com>
2026-02-23 20:27:45 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/index.js
+++ main/app/(tabs)/index.js
@@ -5,7 +5,7 @@ import {Link} from 'expo-router'
 function App(){
   return(
     <View style={{width:'100%',height:'100%',backgroundColor:'blue',alignItems:'center',justifyContent:'center'}}>
-      <Image source={Logo} style={{width:'40%'}}/>
+      <Image source={Logo} style={{width:'40%',height:'40%'}}/>
       <Text style={{color:'white'}}>hello welcome to react</Text>
     <TouchableOpacity style={{backgroundColor:'red',borderRadius:8,alignItems:'center',justifyContent:'center',marginTop:10,width:150,height:50}}><Text style={{color:'white'}}>Hello</Text></TouchableOpacity>
     <Link href="/create">About</Link>
