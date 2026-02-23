ea502ecdd34715638f84a9e4b4e13507f8a3d1ab
co77o <ezra77453@gmail.com>
2026-02-23 17:04:07 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/index.js
+++ main/app/(tabs)/index.js
@@ -4,7 +4,7 @@ import {Text,View,StyleSheet} from 'react-native'
 
 function App(){
   return(
-    <View>
+    <View style={{width:'100%',height:'100%',backgroundColor:'blue',alignItems:'center',justifyContent:'center'}}>
       <Text style={{color:'white'}}>hello welcome to react</Text>
     </View>
     )
