1f58293692283fe2eb78a48a4a522d378dd0d3f7
co77o <ezra77453@gmail.com>
2026-02-24 19:19:01 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/_layout.tsx
+++ main/app/(tabs)/_layout.tsx
@@ -3,11 +3,27 @@ import { Drawer } from 'expo-router/drawer';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
+import {DrawerContentComponentProps} from 'react-navigation/drawer'
+function Custom(props:DrawerContentComponentProps){
+  return(
+    <DrawerContentScrollView {...props}>
+      <View>
+        <TouchableOpacity style={{backgroundColor:'red'}}>hello</TouchableOpacity>
+      </View>
+      <DrawerItemList {...props}/>
+    </DrawerContentScrollView>
+    )
+}
+
+
+
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{flex:1}}>
       
-          <Drawer screenOptions={{
+          <Drawer
+          drawerContent={Custom}
+          screenOptions={{
       drawerActiveTintColor:'red',
       swipeEnabled:true
     }}/>
