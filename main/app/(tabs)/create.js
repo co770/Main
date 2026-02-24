bbf33c2fcd55943103df1d28990aaf89b4fbeb70
co77o <ezra77453@gmail.com>
2026-02-24 11:49:18 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/create.js
+++ main/app/(tabs)/create.js
@@ -1,9 +1,11 @@
-
+import {Link} from 'expo-router'
 import {View,Text} from 'react-native'
 import React from 'react';
 
 export default function Create() {
   return (
-  <View><Text>Create</Text></View>
+  <View><Text>Create</Text>
+  <Link href='/' style={{color:'white'}}>create</Link>
+  </View>
   );
 }
