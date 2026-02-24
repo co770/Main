116129c17ec6def9d25d8b886ac30d23282a3ead
co77o <ezra77453@gmail.com>
2026-02-24 18:12:10 +0100

    Fix Expo Router config and add index.js

--- main/app/(tabs)/_layout.tsx
+++ main/app/(tabs)/_layout.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { Drawer } from 'expo-router/drawer';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Layout() {
   return (
-    <Stack
-    screenOptions={{
-      headerRight:()=>(
-      <View style={{width:150,justifyContent:'space-evenly',flexDirection:'row'
-      }}>
-        <Ionicons name="home" size={24} color="white"/>
-        <Ionicons name="person" size={24} color="white"/>
-      </View>
-      ),
-      headerTitle:'',
-      headerTintColor:'white'
-    }}
-      />
+    <Drawer />
       
       )}
