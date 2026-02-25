0c52349be80b3406ef900a4b18134d02ac3679a8
co77o <ezra77453@gmail.com>
2026-02-26 00:58:29 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import {View,Text} from 'react-native'
+import {View,Text,TextInput} from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 export default function RootLayout() {
   return <Stack
@@ -9,7 +9,7 @@ export default function RootLayout() {
        elevation:5,
      },
      headerTitle:()=>(
-     
+     <View></View>
      ),
      headerRight:()=>(
      <View><Ionicons name='home' size={20} color="white"/></View>
