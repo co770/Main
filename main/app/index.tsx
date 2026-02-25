16f7d0bcc80c92d24cfff7a1da49204fc6af8a82
co77o <ezra77453@gmail.com>
2026-02-25 16:21:37 +0100

    Fix Expo Router config and add index.js

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -10,6 +10,10 @@ export default function Index() {
     >
     <Image source={require('../assets/images/react-logo.png')} style={{width:150,height:150,borderRadius:75}}/>
     <TextInput/>
+    <TextInput/>
+    <TextInput/>
+    <TouchOpacity style={{backgroundColor:'#0f172a',width:200,height:50,color:'white',justifyContent:'center',alignItems:'center'}}><Text>Login</Text></TouchOpacity>
+    
     </View>
   );
 }
