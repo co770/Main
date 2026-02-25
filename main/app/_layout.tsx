b7bd2ff4f48701440eb26b5a26e06a5fea231281
co77o <ezra77453@gmail.com>
2026-02-25 21:34:25 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
-import {View} from 'react-native'
+import {View,Text} from 'react-native'
 export default function RootLayout() {
   return <Stack
    screenOptions={{
      headerStyle:{
        backgroundColor:'black'
-     }
+     },
      headerRight:()=>(
      <View><Text style={{color:'white'}}>Honda</Text></View>
      )
