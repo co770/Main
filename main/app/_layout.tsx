944ae4f06ec45df9d0324406c4ffbca5a4353b87
co77o <ezra77453@gmail.com>
2026-02-25 21:19:04 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -3,6 +3,9 @@ import {View} from 'react-native'
 export default function RootLayout() {
   return <Stack
    screenOptions={{
+     headerStyle:{
+       backgroundColor:'black'
+     }
      headerRight:()=>(
      <View><Text style={{color:'white'}}>Honda</Text></View>
      )
