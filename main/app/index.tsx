a14f935c6645470ca228fde157410c9546ab38e4
co77o <ezra77453@gmail.com>
2026-02-25 16:39:08 +0100

    Fix Expo Router config and add index.js

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -1,4 +1,6 @@
 import { Text, View,TouchableOpacity,TextInput,Image } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 export default function Index() {
   return (
     <View
@@ -9,10 +11,11 @@ export default function Index() {
       }}
     >
     <Image source={require('../assets/images/react-logo.png')} style={{width:150,height:150,borderRadius:75}}/>
-    <TextInput/>
-    <TextInput/>
-    <TextInput/>
-    <TouchableOpacity style={{backgroundColor:'#0f172a',width:200,height:50,color:'white',justifyContent:'center',alignItems:'center'}}><Text>Login</Text></TouchableOpacity>
+    <View style={{flexDirection:'row',width:300,justifyContent:'space-between',borderBottomWidth:1,borderBottomColor:'black',marginBottom:20,alignItems:'center'}}>
+    <TextInput style={{height:40,width:230}}/>
+    <Ionicons name='home' size={24} color="black"/>
+    </View>
+    <TouchableOpacity style={{backgroundColor:'#0f172a',width:280,height:50,color:'white',justifyContent:'center',alignItems:'center',borderRadius:8}}><Text style={{color:'white'}}>Login</Text></TouchableOpacity>
     
     </View>
   );
