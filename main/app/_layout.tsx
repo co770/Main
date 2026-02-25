e6c60c710546b3ec208159e9053b1ce23e3eca31
co77o <ezra77453@gmail.com>
2026-02-25 22:25:11 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import {View,Text} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
 export default function RootLayout() {
   return <Stack
    screenOptions={{
