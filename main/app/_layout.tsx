788dd18888a529c20098a835b2e10a5262075dda
co77o <ezra77453@gmail.com>
2026-02-25 23:00:18 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
        elevation:5,
      },
      headerRight:()=>(
-     <View><Ionicons name='home' size={22} color="white"/></View>
+     <View><Ionicons name='home' size={20} color="white"/></View>
      )
    }}
   
