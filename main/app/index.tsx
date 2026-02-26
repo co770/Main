d8add051cb45172979ec56a098da34b695f5525f
co77o <ezra77453@gmail.com>
2026-02-26 14:50:31 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
             flexDirection: "row",
             padding: 15,
             marginBottom:10,
-            elevation:2,
+            
             alignItems: "center",
             borderWidth:1,
             borderColor:'#ccc',
