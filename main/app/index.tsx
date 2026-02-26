256034cd526963a99177c2a70d534e626af47391
co77o <ezra77453@gmail.com>
2026-02-26 15:35:24 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text, FlatList,TouchableOpacity} from "react-native";
 import {Ionicons} from '@expo/vector-icons'
 const users = [
-  { id: "1", name: "John",online:true},
+  { id: "1", name: "John",online:true,message:"hello ezra,how are you i hope you are good"},
   { id: "2", name: "Mary", online:false},
   { id: "3", name: "David", online:false },
 ];
