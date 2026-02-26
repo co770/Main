d7a47b8b477e1eb74e761d9a3ad1ab51b580b8c1
co77o <ezra77453@gmail.com>
2026-02-26 15:36:10 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text, FlatList,TouchableOpacity} from "react-native";
 import {Ionicons} from '@expo/vector-icons'
 const users = [
   { id: "1", name: "John",online:true,message:"hello ezra,how are you i hope you are good"},
-  { id: "2", name: "Mary", online:false},
-  { id: "3", name: "David", online:false },
+  { id: "2", name: "Mary", online:false,message:"have you sent the 1million?"},
+  { id: "3", name: "David", online:false,message:'hi' },
 ];
 
 export default function Index() {
