8cef3d20e041f58ad89d5a4bd75371f809e725d8
co77o <ezra77453@gmail.com>
2026-02-26 13:07:03 +0100

    sync

--- main/app/modal.js
+++ main/app/modal.js
@@ -1,7 +1,7 @@
 import { Text, View,TouchableOpacity,TextInput,Image } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
-export default function Index() {
+export default function Modal() {
   return (
     <View
       style={{
