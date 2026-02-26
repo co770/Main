ce7c8c9448949efb6acef9e5377d9ea78e02d83b
co77o <ezra77453@gmail.com>
2026-02-26 15:08:38 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         data={users}
         renderItem={({ item }) => (
         <View style={{flexDirection:'row'}}>
-          <Ionicons name='profile' size={25}/>
+          <Ionicons name='person-circle' size={25} color="grey"/>
           <Text style={{ fontSize: 18, marginBottom: 10 }}>
             {item.name}
           </Text>
