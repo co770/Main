8ec51ea6bd04ba1243b3faa908aa0ea591a858a5
co77o <ezra77453@gmail.com>
2026-02-26 13:06:36 +0100

    sync

---
+++ main/app/modal.js
@@ -0,0 +1,34 @@
+import { Text, View,TouchableOpacity,TextInput,Image } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        
+        alignItems: "center",
+      }}
+    >
+    <Image source={require('../assets/images/react-logo.png')} style={{width:150,height:150,borderRadius:75,marginTop:150}}/>
+    
+    
+    
+    
+    <View style={{flexDirection:'row',width:300,gap:5,borderBottomWidth:1,borderBottomColor:'brown',marginBottom:20,alignItems:'center'}}>   
+    
+    <Ionicons name='person' size={21} color="black"  style={{marginLeft:3}}/>
+    <TextInput placeholder="Username" style={{height:40,width:230}}/>
+   
+    </View>
+    
+    
+    
+    
+    
+    
+    <TouchableOpacity style={{backgroundColor:'#0f172a',width:280,height:50,color:'white',justifyContent:'center',alignItems:'center',borderRadius:8}}><Text style={{color:'white'}}>Login</Text></TouchableOpacity>
+    
+    </View>
+  );
+}
