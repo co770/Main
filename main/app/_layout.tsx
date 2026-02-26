5df84e7d96c432a73425bbb44f8420816b01c3e5
co77o <ezra77453@gmail.com>
2026-02-26 14:35:04 +0100

    sync

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -2,22 +2,7 @@ import { Stack } from "expo-router";
 import {View,Text,TextInput} from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 export default function RootLayout() {
-  return <Stack
-   screenOptions={{
-     headerStyle:{
-       backgroundColor:'',
-       elevation:5,
-     },
-   }}
-  >
-  
-  <Stack.Screen
-  name="modal"
-  options={{
-    presentation: 'modal',
-    animation: 'slide_from_bottom', // forces bottom-up animation on Android
-  }}
-/>
-
-  </Stack>;
+  return(
+    <Stack/>
+    )
 }
