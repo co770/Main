c47e6786ebfd0861b95d0bd42d05b79bf4b6140a
co77o <ezra77453@gmail.com>
2026-02-27 16:47:32 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import {Text,View} from 'react-native'
+import {Text,View,Button} from 'react-native'
 import Animated,{useSharedValue,withTiming} from 'react-native-reanimated'
 
 
 function Index(){
   const width=useSharedValue(150)
-  const Height=useSharedValue(150)
+  const height=useSharedValue(150)
+  
+  
+  
+const StartAnimation=()=>{
+  width.value=withTiming(Math.floor(Math.random()*300)+100)
+  height.value=withTiming(Math.floor(Math.random()*300)+100)
+}
+  
   return(
     
     
