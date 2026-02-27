c9cfe81dd580be10cbd47861c22688ec269a024c
co77o <ezra77453@gmail.com>
2026-02-27 16:43:01 +0100

    sync

--- main/app/index.tsx
+++ main/app/index.tsx
@@ -1,3 +1,14 @@
 import React from 'react'
 import {Text,View} from 'react-native'
-import Animated,{useSharedValue} from 'react-native-reanimated'
+import Animated,{useSharedValue,withTiming} from 'react-native-reanimated'
+
+
+function Index(){
+  const width=useSharedValue(150)
+  const Height=useSharedValue(150)
+  return(
+    
+    
+    
+    )
+}
